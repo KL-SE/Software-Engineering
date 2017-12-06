d2c2bff4370ef89d582fed25a31e9ad3baf29f31
Kitson Lim <kittykittykid@gmail.com>
2017-12-06 23:03:33 +0800

    Initial UI Prototype

---
+++ Over Surgery System (UI)/Persistent/MenuBanner.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace OverSurgerySystem.UI.Persistent
+{
+    /// <summary>
+    /// Interaction logic for MenuBanner.xaml
+    /// </summary>
+    public partial class MenuBanner : Page
+    {
+        public MenuBanner()
+        {
+            InitializeComponent();
+            Loaded += Initialize;
+
+            LogoutButton.MouseLeftButtonDown += new MouseButtonEventHandler( DoLogout );
+        }
+
+        public void Initialize( object sender , RoutedEventArgs e)
+        {
+        }
+
+        private void DoLogout( object sender , MouseButtonEventArgs e )
+        {
+            Welcome.Text            = "Welcome!";
+            PageName.Text           = "Sample Page";
+            Username.Visibility     = Visibility.Collapsed;
+            StaffId.Visibility      = Visibility.Collapsed;
+            StaffIdDesc.Visibility  = Visibility.Collapsed;
+            LogoutButton.Text       = "Log In";
+        }
+    }
+}
